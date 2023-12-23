62c402494794e6b0580668eab3fd3be97aa4fc70
ADOVO666 <nikita.mel2004@gmail.com>
2023-12-23 18:14:59 +0500

    Com1

---
+++ lab2/lab2.cpp
@@ -0,0 +1,117 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <vector>
+
+
+// МЕЛЬНИЧЕНКО НИКИТА АЛЕКСАНДРОВИЧ РИ-221055
+using namespace std;
+//Изменение для гита
+int main()
+{
+    //ОБЪЯВЛЕНИЕ ПЕРЕМЕННЫХ И МАССИВОВ
+    setlocale(LC_ALL, "RU");
+    int i, j, arr11[10], arr12[10], arr13[5], arr14[10], arr21[5][5];
+    cout << "Одномерный массив для пункта 1\n" << "[";
+
+    //ЗАДАНИЕ 1.1
+    for (i = 0; i < 10; i++)
+    {
+        cout << arr11[i] << " ";
+    }
+    cout << "]\n";
+
+    //ЗАДАНИЕ 1.2
+    srand(time(0));
+    cout << "Одномерный массив для пункта 2\n" << "[";
+    for (i = 0; i < 10; i++)
+    {
+        arr12[i] = rand() % 21 - 10;
+        cout << arr12[i] << " ";
+    }
+    cout << "]\n";
+
+    //ЗАДАНИЕ 1.3
+    int PersonNumber;
+    cout << "Введите пять чисел для массива из 3-го пункта\n";
+    for (i = 0; i < 5; i++)
+    {
+        cin >> PersonNumber;
+        arr13[i] = PersonNumber;
+
+    }
+    cout << "Одномерный массив для пункта 3\n" << "[";
+    for (i = 0; i < 5; i++)
+    {
+        cout << arr13[i] << " ";
+    }
+    cout << "]\n";
+
+    //ЗАДАНИЕ 1.4
+    for (i = 0; i < 10; i++)
+    {
+        if (arr12[i] % 2 == 0)
+        {
+            arr14[i] = arr12[i] * 3;
+        }
+        else
+            arr14[i] = arr12[i] * 5;
+
+    }
+
+    cout << "Одномерный массив для пункта 4\n" << "[";
+    for (i = 0; i < 10; i++)
+    {
+        cout << arr14[i] << " ";
+    }
+    cout << "]\n";
+
+
+    //ЗАДАНИЕ 2.1
+    cout << "Двумерный массив для пункта 1\n";
+    srand(time(0));
+    for (i = 0; i < 5; i++) {
+        cout << "[";
+        for (j = 0; j < 5; j++) {
+            arr21[i][j] = rand() % 21 - 10;
+            cout << arr21[i][j] << " ";
+        }
+        cout << "]" << endl;
+    }
+    cout << endl;
+
+    //ЗАДАНИЕ 2.2
+    int sum = 0;
+    for (i = 0; i < 5; i++) {
+        sum = 0;
+        for (j = 0; j < 5; j++) {
+            sum += arr21[i][j];
+
+        }
+
+        cout << "Сумма " << i << "-й строки: " << sum << endl;
+    }
+
+
+
+    //ЗАДАНИЕ 2.3
+
+    for (i = 0; i < 5; i++) {
+        cout << "[";
+        for (j = 0; j < 5; j++) {
+            if (arr21[i][j] < 0) {
+                arr21[i][j] = 0;
+            }
+            cout << arr21[i][j] << " ";
+        }
+        cout << "]" << endl;
+    }
+
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ lab3/lab3.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "RU");
+
+	ifstream file1("D:\\newfolder\\test1.txt");
+	ifstream file2("D:\\newfolder\\test2.txt");
+	ifstream file3("D:\\newfolder\\test3.txt");
+	if (!file1.is_open()) {
+		cout << "Failed to open file 1\n";
+		return 1;
+	}
+	if (!file2.is_open()) {
+		cout << "Failed to open file 1\n";
+		return 1;
+	}
+	if (!file3.is_open()) {
+		cout << "Failed to open file 1\n";
+		return 1;
+	}
+	string s1, s2, s3;
+	ofstream fileres("D:\\newfolder\\resault.txt");
+	cout << "Данные первого файла:\n";
+	while (getline(file1, s1)) {
+		fileres << stoi(s1) * 2 << endl;
+		cout << stoi(s1) << endl;
+	}
+	cout << "Данные второго файла:\n";
+	while (getline(file2, s2)) {
+		fileres << stoi(s2) * 2 << endl;
+		cout << stoi(s2) << endl;
+	}
+	cout << "Данные третьего файла:\n";
+	while (getline(file3, s3)) {
+		fileres << stoi(s3) * 2 << endl;
+		cout << stoi(s3) << endl;
+	}
+	file1.close();
+	file2.close();
+	file3.close();
+	fileres.close();
+	return 0;
+}
